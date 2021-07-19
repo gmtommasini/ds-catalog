567df473af5bf3124f9daf1d6144ede13e90bcc2
Guilherme Tommasini <gmtommasini@gmail.com>
2021-07-19 11:32:30 -0400

    pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,13 +1,12 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,19 +23,26 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-//		List<CategoryDTO> listDTO = list.stream().map( (obj)-> new CategoryDTO(obj) ).collect(Collectors.toList());		
-//		return 	listDTO;
-		return list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
-	}
-
-	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Category obj = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Category '" + id + "' not found"));
 		return new CategoryDTO(obj);
 	}
+	
+//	@Transactional(readOnly = true)
+//	public List<CategoryDTO> findAll() {
+//		List<Category> list = repository.findAll();
+////		List<CategoryDTO> listDTO = list.stream().map( (obj)-> new CategoryDTO(obj) ).collect(Collectors.toList());		
+////		return 	listDTO;
+//		return list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+//	}
+	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> page = repository.findAll(pageRequest);
+		return page.map(obj -> new CategoryDTO(obj));
+	}
+
 
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
@@ -73,5 +79,6 @@ public class CategoryService {
 		}
 
 	}
+
 
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,29 @@
 insert into tb_categories (name, created_at) values		('Books', NOW());
 insert into tb_categories (name, created_at) values		('Electronics', NOW());
 insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+insert into tb_categories (name, created_at) values		('Books', NOW());
+insert into tb_categories (name, created_at) values		('Electronics', NOW());
+insert into tb_categories (name, created_at) values		('Computers', NOW());
+
+
