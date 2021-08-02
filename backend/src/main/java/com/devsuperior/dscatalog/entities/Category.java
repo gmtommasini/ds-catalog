4a2fc7323cda29f16efe5749a55df8613d441bcc
gmtommasini <gmtommasini@gmail.com>
2021-08-02 15:57:57 -0400

    User and Role ORM mapping and seed

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -27,7 +30,8 @@ public class Category implements Serializable{
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE") //UTC
 	private Instant updatedAt;
 	
-	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	/********** Constructors *********/
 	public Category() {}
 
@@ -64,6 +68,10 @@ public class Category implements Serializable{
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
+	}
+	
+	public Set<Product> getProducts(){
+		return products;
 	}
 
 	/********** Standard methods *********/
