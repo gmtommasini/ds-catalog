80280083e62048eb44b3c9a1d4b090172276b688
gmtommasini <gmtommasini@gmail.com>
2021-08-12 18:21:57 -0400

    H2 configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,12 +15,18 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer // process configuration to implements OAuth2 resource server
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private JwtTokenStore tokenStore;
 
+	// open area
 	private static final String[] PUBLIC = {
-			"/oauth/token" /* add more as needed */ }; // open area
+			"/oauth/token",
+			"/h2-console/**"/* add more as needed */ };
+	// /** means everything from that resource
 	private static final String[] OPERATOR_ADMIN = {
 			"/products/**", 
 			"/categories/**" }; // only for admins and operators
@@ -32,6 +41,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		//H2
+		if(Arrays.asList(env.getActiveProfiles() ).contains("test")){
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 				.antMatchers(PUBLIC).permitAll() // for all resources listed in PUBLIC, free access
 				.antMatchers(HttpMethod.GET, OPERATOR_ADMIN).permitAll() // free access using GET in the resources listed in OPERATOR_ADMIN
