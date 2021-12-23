2cbb5288e423f9153c5d5afec05f74c70582951d
tommasgu <tommasgu@mpac.ca>
2021-12-23 16:44:16 -0500

    aula 07-16

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,17 @@
 // import React from 'react'; // This import is not necessary withing modules after React v17
-import './assets/styles/custom.scss'
+import './assets/styles/custom.scss';
+import Navbar from './components/Navbar';
 import './App.css';
 
+// we can also use lambda notation:
+// const App =() => {
 function App() {
-  return (
-    <h1 >Hello DSCatalog</h1>
-  );
+   return (
+      <>
+         <Navbar />
+         <h1>Hello DSCatalog</h1>
+      </>
+   ); // A JSX connot return more than one element, do we must wrap it in a div or just in a fragment, as we did here
 }
 
 export default App;
